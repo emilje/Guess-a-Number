b1e5f2ba5c788224ceb87a587347585a0ffd7993
emil.je <emil.jerkovic@gmail.com>
2020-11-20 00:22:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 
 
 console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "Correct Number !";
+document.querySelector(".message").textContent = "Start Guessing!";
 
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
+document.querySelector(".number").textContent = "?";
+document.querySelector(".score").textContent = 20;
 
-document.querySelector(".guess").value = 23;
+document.querySelector(".guess").value = "";
 console.log(document.querySelector(".guess").value);
 
 
